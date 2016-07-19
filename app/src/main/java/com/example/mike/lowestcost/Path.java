a234318049419a27562dfc695650c3d84740014a
Mike Mueller <mikemueller04@hotmail.com>
2016-07-19 17:00:45 -0400

    passed lowestSumFoundFromThreeRows

--- app/src/main/java/com/example/mike/lowestcost/Path.java
+++ app/src/main/java/com/example/mike/lowestcost/Path.java
@@ -17,25 +17,40 @@ public class Path {
     }
 
     public static int[][] pathTotals (int[][] grid) {
-        int[][] gridPathTotals = grid;
+        int[][] gridPathTotals = new int[grid.length][grid[0].length];
+
+        //populate the first column
+        for (int i = 0; i < grid[0].length; i++) {
+            gridPathTotals[i][0] = grid[i][0];
+        }
 
         for (int column = 1; column < grid[0].length; column++) {
 
             for (int row = 0; row < grid.length; row++) {
-                int previousRowToAdd = 0;
+                int previousRowToAdd;
+                int previousRowToSubtract;
 
-                if (row < grid.length - 1 && grid[row][column - 1] < grid[row + 1][column - 1]) {
+                if (row == 0) {
+                    previousRowToSubtract = grid.length - 1;
+                } else {
+                    previousRowToSubtract = row;
+                }
+
+                if (row == grid.length - 1) {
+                    previousRowToAdd = 0;
+                } else {
                     previousRowToAdd = row;
                 }
 
-                if (row < grid.length - 1  && grid[row][column - 1] > grid[row + 1][column - 1]) {
-                    previousRowToAdd = row + 1;
-                }
+                int upperLeft = gridPathTotals[previousRowToAdd + 1][column - 1];
+                int left = gridPathTotals[row][column -1];
+                int lowerLeft = gridPathTotals[previousRowToSubtract - 1][column - 1];
 
-                gridPathTotals[row][column] = grid[row][column] + grid[previousRowToAdd][column - 1];
+                int lowestCellValue = Math.min(upperLeft ,Math.min(left, lowerLeft));
+
+                gridPathTotals[row][column] = lowestCellValue + grid[row][column];
             }
         }
-
         return gridPathTotals;
     }
 
