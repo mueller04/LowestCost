10a9474d6cf7e7e46520eee184bb6204c778b0a8
Mike Mueller <mikemueller04@hotmail.com>
2016-07-19 13:47:53 -0400

    wip array out of bounds

--- app/src/main/java/com/example/mike/lowestcost/Path.java
+++ app/src/main/java/com/example/mike/lowestcost/Path.java
@@ -2,11 +2,26 @@ package com.example.mike.lowestcost;
 
 public class Path {
 
-    public static int navigate(int[] row) {
-        int leastCostSum =0 ;
-        for (int i = 0; i < row.length; i++) {
-            leastCostSum += row[i];
+    public static int navigate(int[][] grid) {
+        int leastCostSum = 0;
+
+
+        for (int row = 0; row < grid.length; row++) {
+
+            for (int column = 0; column < grid[0].length; column++) {
+                
+                if (grid[row + 1][column] > grid[row][column]) {
+                    leastCostSum += grid[row + 1][column];
+                } else {
+                    leastCostSum += grid[row][column];
+                }
+
+                leastCostSum += grid[row][column];
+
+            }
         }
+
+
         return leastCostSum;
     }
 
--- app/src/test/java/com/example/mike/lowestcost/PathTest.java
+++ app/src/test/java/com/example/mike/lowestcost/PathTest.java
@@ -5,17 +5,6 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class PathTest {
-    @Test
-    public void pathSumFoundFromASingleRow() {
-        //arrange
-        int[] row = {1, 3};
-
-        //act
-        int pathSumResult = Path.navigate(row);
-
-        //assert
-        assertEquals(4, pathSumResult);
-    }
 
     @Test
     public void lowestSumFoundFromTwoRows() {
