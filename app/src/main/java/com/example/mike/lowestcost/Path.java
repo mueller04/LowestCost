3f9664e7492d84879cc55dca447e9e8e07e59304
Mike Mueller <mikemueller04@hotmail.com>
2016-07-19 15:47:52 -0400

    passed lowestSumFoundFromTwoRows

--- app/src/main/java/com/example/mike/lowestcost/Path.java
+++ app/src/main/java/com/example/mike/lowestcost/Path.java
@@ -3,26 +3,41 @@ package com.example.mike.lowestcost;
 public class Path {
 
     public static int navigate(int[][] grid) {
-        int leastCostSum = 0;
 
+        int[][] gridPathTotals = pathTotals(grid);
+        int columnLength = grid[0].length - 1;
+        int leastCostSum = gridPathTotals[0][columnLength];
 
-        for (int row = 0; row < grid.length; row++) {
+        for (int i = 1; i < grid.length; i++) {
+            if (gridPathTotals[i][columnLength] < leastCostSum) {
+                leastCostSum = gridPathTotals[i][columnLength];
+            }
+        }
+        return leastCostSum;
+    }
 
-            for (int column = 0; column < grid[0].length; column++) {
-                
-                if (grid[row + 1][column] > grid[row][column]) {
-                    leastCostSum += grid[row + 1][column];
-                } else {
-                    leastCostSum += grid[row][column];
+    public static int[][] pathTotals (int[][] grid) {
+        int[][] gridPathTotals = grid;
+
+        for (int column = 1; column < grid[0].length; column++) {
+
+            for (int row = 0; row < grid.length; row++) {
+                int previousRowToAdd = 0;
+
+                if (row < grid.length - 1 && grid[row][column - 1] < grid[row + 1][column - 1]) {
+                    previousRowToAdd = row;
                 }
 
-                leastCostSum += grid[row][column];
+                if (row < grid.length - 1  && grid[row][column - 1] > grid[row + 1][column - 1]) {
+                    previousRowToAdd = row + 1;
+                }
 
+                gridPathTotals[row][column] = grid[row][column] + grid[previousRowToAdd][column - 1];
             }
         }
 
-
-        return leastCostSum;
+        return gridPathTotals;
     }
+
 
 }
