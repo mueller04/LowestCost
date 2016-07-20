76018a8c363f7919f3ed439e88e2bf54b4a54a19
Mike Mueller <mikemueller04@hotmail.com>
2016-07-20 15:05:31 -0400

    refactored

--- app/src/main/java/com/example/mike/lowestcost/Path.java
+++ app/src/main/java/com/example/mike/lowestcost/Path.java
@@ -3,50 +3,38 @@ package com.example.mike.lowestcost;
 public class Path {
 
     public static PathResult navigate(int[][] grid) {
+        PathResult pathResult = new PathResult();
+        int rowLength = grid.length;
+        int columnLength = grid[0].length;
 
-        if (grid.length > 10) {
-            boolean isValidRows = false;
-            PathResult pathResult = new PathResult();
-            pathResult.setIsValidRows(isValidRows);
+        pathResult = validateRowLength(rowLength, pathResult);
+        pathResult = validateColumnLength(columnLength, pathResult);
+        if (!pathResult.getIsValidRows() || !pathResult.getIsValidColumns()) {
             return pathResult;
         }
 
-        if (grid[0].length < 5 || grid[0].length > 100) {
-            boolean isValidColumns = false;
-            PathResult pathResult = new PathResult();
-            pathResult.setIsValidColumns(isValidColumns);
-            return pathResult;
-        }
-
-        PathResult pathResult = pathTotals(grid);
+        pathResult = pathTotals(grid);
         int[][] gridPathTotals = pathResult.getGridPathTotals();
-        int columnLength = grid[0].length - 1;
-        int leastCostSum = gridPathTotals[0][columnLength];
 
-        for (int i = 1; i < grid.length; i++) {
-            if (gridPathTotals[i][columnLength] < leastCostSum) {
-                leastCostSum = gridPathTotals[i][columnLength];
-            }
-        }
+        int leastCostSumForGrid = getLeastPathSumForGrid(gridPathTotals, rowLength, columnLength);
 
-        pathResult.setLeastCostSum(leastCostSum);
+        pathResult.setLeastCostSum(leastCostSumForGrid);
         return pathResult;
     }
 
     public static PathResult pathTotals (int[][] grid) {
         int[][] gridPathTotals = new int[grid.length][grid[0].length];
         int rowLength = grid.length;
-        boolean[] isRowPathEnded = new boolean[rowLength];
-        for (int i = 0; i < rowLength; i++) {
-            isRowPathEnded[i] = false;
-        }
+        int columnLength = grid[0].length;
+
+        boolean[] isRowPathEnded = initializeRowBoolForEachRow(rowLength);
 
         //populate the first column
-        for (int i = 0; i < grid.length; i++) {
+        for (int i = 0; i < rowLength; i++) {
             gridPathTotals[i][0] = grid[i][0];
         }
 
-        for (int column = 1; column < grid[0].length; column++) {
+        for (int column = 1; column < columnLength; column++) {
 
             for (int row = 0; row < rowLength; row++) {
 
@@ -71,16 +59,7 @@ public class Path {
             }
         }
 
-        boolean ispathComplete = false;
-        if (!ispathComplete) {
-            for (int row = 0; row < rowLength; row++) {
-                if (isRowPathEnded[row] == false) {
-                    ispathComplete = true;
-                }
-            }
-        }
-
-
+        boolean ispathComplete = oneOrMorePathsCompleted(isRowPathEnded, rowLength);
 
         PathResult pathResult = new PathResult();
         pathResult.setPathComplete(ispathComplete);
@@ -102,6 +81,54 @@ public class Path {
         } else {
             return currentRow + 1;
         }
+    }
+
+    private static boolean[] initializeRowBoolForEachRow(int rowLength) {
+        boolean[] isRowPathEnded = new boolean[rowLength];
+        for (int i = 0; i < rowLength; i++) {
+            isRowPathEnded[i] = false;
+        }
+        return isRowPathEnded;
+    }
+
+    private static boolean oneOrMorePathsCompleted(boolean[] isRowPathEnded, int rowLength) {
+        boolean ispathComplete = false;
+        if (!ispathComplete) {
+            for (int row = 0; row < rowLength; row++) {
+                if (isRowPathEnded[row] == false) {
+                    ispathComplete = true;
+                }
+            }
+        }
+        return ispathComplete;
+    }
+
+    private static PathResult validateRowLength(int rowLength, PathResult pathResult) {
+        if (rowLength > 10) {
+            boolean isValidRows = false;
+            pathResult.setIsValidRows(isValidRows);
+        }
+        return pathResult;
+    }
+
+    private static PathResult validateColumnLength(int columnLength, PathResult pathResult) {
+        if (columnLength < 5 || columnLength > 100) {
+            boolean isValidColumns = false;
+            pathResult.setIsValidColumns(isValidColumns);
+        }
+        return pathResult;
+    }
+
+    private static int getLeastPathSumForGrid(int[][] gridPathTotals, int rowLength, int columnLength) {
+        int leastCostSumForRow = gridPathTotals[0][columnLength - 1];
+        int leastCostSumForGrid = leastCostSumForRow;
+
+        for (int row = 1; row < rowLength; row++) {
+            if (gridPathTotals[row][columnLength - 1] < leastCostSumForRow) {
+                leastCostSumForGrid = gridPathTotals[row][columnLength - 1];
+            }
+        }
+        return leastCostSumForGrid;
     }
 
 
